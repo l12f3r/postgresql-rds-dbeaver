f8aac7127a3c1981d76b674c8d9f72cce7a01966
Luiz Ferreira <luizferreira@outlook.com>
2022-02-25 09:39:41 +0000

    Removed egress connection from security group

--- main.tf
+++ main.tf
@@ -35,13 +35,6 @@ resource "aws_security_group" "ourDBSecG" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  egress {
-    from_port = 5432
-    to_port = 5432
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 resource "aws_db_parameter_group" "ourDBParamGroup" {
